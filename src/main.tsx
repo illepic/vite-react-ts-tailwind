51117a8ad5b83a7802cb9820aa9bbdcc2061be40
Christopher Bloom <bloomcb@gmail.com>
2021-08-22 13:22:16 -0700

    remove git conflicts

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
-// import "./index.css";
-=======
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./index.css";
 import Landing from "./Landing";
->>>>>>> Example for typing api response, router
 import App from "./App";
 
 ReactDOM.render(
